185aa941c6141cdc1044a4be1b44db2d3079eb47
nighttwo1 <43779571+nighttwo1@users.noreply.github.com>
2018-11-28 22:16:08 +0900

    Add files via upload

---
+++ report_card1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+	int kor, eng, math;
+	int sum = 0;
+
+	math = 80;
+	eng = 100;
+	kor = 90;
+	sum = 80 + 100 + 90;
+
+	printf("This program print report card.\n");
+	return 0;
+
+}
